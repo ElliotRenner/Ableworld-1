0def712c902181b80dc7970c5c1d1990f88f6ee8
Banana-Goats <143899844+Banana-Goats@users.noreply.github.com>
2023-09-22 09:27:48 +0100

    Update EXP Builder.cs

--- EXP Builder.cs
+++ EXP Builder.cs
@@ -1,17 +1,22 @@
 using System.Net;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Phone_EXP_Builder
 {
     public partial class Form1 : Form
     {
         private Dictionary<string, string> nameToNumberDictionary;
-        private int maxPairsPerTab;
+        //private int maxPairsPerTab;
 
         public Form1()
         {
             InitializeComponent();
             GenerateOutputButton.Click += GenerateOutputButton_Click;
+            nameToNumberDictionary = new Dictionary<string, string>();
+            string csvUrl = "https://raw.githubusercontent.com/Banana-Goats/Ableworld/main/Contacts.csv";
+            PopulateDictionaryFromCsvAsync(csvUrl);
+
         }
 
         private void GenerateOutputButton_Click(object sender, EventArgs e)
@@ -83,5 +88,100 @@ namespace Phone_EXP_Builder
             }
         }
 
+        private void LookupNumbersButton_Click(object sender, EventArgs e)
+        {
+            int maxPairsPerTab = 60;
+
+            foreach (Control control in tabControl1.Controls)
+            {
+                if (control is TabPage tabPage)
+                {
+                    for (int i = 1; i <= maxPairsPerTab; i++)
+                    {
+                        string nameTextBoxName = $"name{i}";
+                        string numberTextBoxName = $"number{i}";
+
+                        // Attempt to get the values from the corresponding text boxes
+                        string name = tabPage.Controls[nameTextBoxName]?.Text;
+
+                        if (!string.IsNullOrWhiteSpace(name))
+                        {
+                            if (nameToNumberDictionary.TryGetValue(name, out string number))
+                            {
+                                tabPage.Controls[numberTextBoxName].Text = number;
+                            }
+                            else
+                            {
+                                MessageBox.Show($"No number found for name: {name}");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private void PopulateDictionary()
+        {
+            // Clear the dictionary to start fresh
+            nameToNumberDictionary.Clear();
+
+            // Path to your CSV file
+            string csvFilePath = "C:\\\\Users\\erenner\\Downloads\\Contacts.csv";
+
+            try
+            {
+                // Read all lines from the CSV file
+                string[] lines = File.ReadAllLines(csvFilePath);
+
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(',');
+
+                    if (parts.Length == 2)
+                    {
+                        string name = parts[0].Trim();
+                        string number = parts[1].Trim();
+                        nameToNumberDictionary[name] = number;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while reading the CSV file: {ex.Message}");
+            }
+        }
+
+        private async Task PopulateDictionaryFromCsvAsync(string csvUrl)
+        {
+            // Clear the dictionary to start fresh
+            nameToNumberDictionary.Clear();
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Download the CSV content
+                    string csvContent = await client.GetStringAsync(csvUrl);
+
+                    // Parse CSV content
+                    string[] lines = csvContent.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (string line in lines)
+                    {
+                        string[] parts = line.Split(',');
+
+                        if (parts.Length == 2)
+                        {
+                            string name = parts[0].Trim();
+                            string number = parts[1].Trim();
+                            nameToNumberDictionary[name] = number;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while reading the CSV from {csvUrl}: {ex.Message}");
+            }
+        }
     }
 }
